94940ad83c70dc34137e4db70616e8f3d8a5b97b
Mihailo <mihailo@MacBook-Pro.local>
2025-07-25 15:43:19 +0200

    trigger properties

--- nodes/AimfoxTrigger/AimfoxTrigger.node.ts
+++ nodes/AimfoxTrigger/AimfoxTrigger.node.ts
@@ -7,7 +7,7 @@ import {
 	NodeConnectionType,
 } from 'n8n-workflow';
 
-import { workspaceFields } from './properties/workspace/fields';
+import { aimfoxTriggerProperties } from './properties';
 
 export class AimfoxTrigger implements INodeType {
 	description: INodeTypeDescription = {
@@ -43,62 +43,7 @@ export class AimfoxTrigger implements INodeType {
 				path: 'webhook',
 			},
 		],
-		properties: [
-			{
-				displayName: 'Trigger On',
-				name: 'events',
-				type: 'multiOptions',
-				required: true,
-				default: [],
-				options: [
-					{
-						name: 'Account Logged In Event',
-						value: 'account_logged_in',
-					},
-					{
-						name: 'Account Logged Out Event',
-						value: 'account_logged_out',
-					},
-					{
-						name: 'Connect Accepted Event',
-						value: 'accepted',
-					},
-					{
-						name: 'Connect Sent Event',
-						value: 'connect',
-					},
-					{
-						name: 'Inmail Reply Event',
-						value: 'inmail_reply',
-					},
-					{
-						name: 'Inmail Sent Event',
-						value: 'inmail',
-					},
-					{
-						name: 'Message Request Sent Event',
-						value: 'message_request',
-					},
-					{
-						name: 'Message Sent Event',
-						value: 'message',
-					},
-					{
-						name: 'New Connection Event',
-						value: 'new_connection',
-					},
-					{
-						name: 'New Reply Event',
-						value: 'reply',
-					},
-					{
-						name: 'Profile Viewed Event',
-						value: 'view',
-					},
-				],
-			},
-			...workspaceFields,
-		],
+		properties: aimfoxTriggerProperties,
 	};
 
 	webhookMethods = {
