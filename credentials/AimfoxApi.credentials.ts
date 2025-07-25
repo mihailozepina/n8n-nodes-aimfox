c4148ac490f7674e76ac9f72606329a7f9764c3c
Mihailo <mihailo@MacBook-Pro.local>
2025-07-25 10:20:25 +0200

    auth fix

--- credentials/AimfoxApi.credentials.ts
+++ credentials/AimfoxApi.credentials.ts
@@ -3,8 +3,6 @@ import { IAuthenticateGeneric, ICredentialType, INodeProperties } from 'n8n-work
 export class AimfoxApi implements ICredentialType {
 	name = 'aimfoxApi';
 	displayName = 'Aimfox API';
-	// Uses the link to this tutorial as an example
-	// Replace with your own docs links when building your own nodes
 	documentationUrl =
 		'https://docs.n8n.io/integrations/creating-nodes/build/declarative-style-node/';
 	properties: INodeProperties[] = [
@@ -16,12 +14,12 @@ export class AimfoxApi implements ICredentialType {
 			default: '',
 		},
 	];
-	authenticate = {
+	authenticate: IAuthenticateGeneric = {
 		type: 'generic',
 		properties: {
 			qs: {
 				appid: '={{$credentials.apiKey}}',
 			},
 		},
-	} as IAuthenticateGeneric;
+	};
 }
